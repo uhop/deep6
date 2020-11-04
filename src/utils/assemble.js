e866f0cec095bd653d637238a8865e8d7919cdc6
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-03 22:38:33 -0600

    Added more types.

--- src/utils/assemble.js
+++ src/utils/assemble.js
@@ -1,4 +1,4 @@
-import {Unifier, Variable, _} from '../unify.js';
+import {Unifier, Variable} from '../unify.js';
 import walk from './walk.js';
 
 const empty = {};
@@ -28,12 +28,41 @@ const postProcess = init =>
   };
 
 const processObject = (val, context) => {
-  if (val === _) {
-    context.stackOut.push(val);
-  } else {
-    const stack = context.stack;
-    stack.push(new walk.Command(postProcess({}), val));
-    Object.keys(val).forEach(k => stack.push(val[k]));
+  const stack = context.stack;
+  stack.push(new walk.Command(postProcess({}), val));
+  Object.keys(val).forEach(k => stack.push(val[k]));
+};
+
+const postProcessMap = context => {
+  const stackOut = context.stackOut,
+    s = this.s;
+  let j = stackOut.length - 1;
+  main: {
+    const result = Array.from(s.values()).some(v => {
+      const t = stackOut[j--];
+      return typeof t == 'number' && isNaN(t) ? typeof v == 'number' && !isNaN(v) : v !== t;
+    });
+    if (result) break main;
+    const l = stackOut.length - 1 - j;
+    if (l) {
+      stackOut.splice(-l, l, s);
+    } else {
+      stackOut.push(s);
+    }
+    return;
+  }
+  const t = new Map();
+  for (const key of s.keys()) {
+    t.set(key, stackOut.pop());
+  }
+  stackOut.push(t);
+};
+
+const processMap = (val, context) => {
+  const stack = context.stack;
+  stack.push(new walk.Command(postProcessMap, val));
+  for (const value of val.values()) {
+    stack.push(value);
   }
 };
 
@@ -47,13 +76,9 @@ const registry = [
     },
     Array,
     function processArray(val, context) {
-      if (val === _) {
-        context.stackOut.push(val);
-      } else {
-        const stack = context.stack;
-        stack.push(new walk.Command(postProcess([]), val));
-        Object.keys(val).forEach(k => stack.push(val[k]));
-      }
+      const stack = context.stack;
+      stack.push(new walk.Command(postProcess([]), val));
+      Object.keys(val).forEach(k => stack.push(val[k]));
     },
     Variable,
     function processVariable(val, context) {
@@ -72,6 +97,28 @@ const registry = [
     processOther
   ],
   filters = [];
+
+// add more exotic types
+
+const addType = (Type, process) => typeof Type == 'function' && registry.push(Type, process || processOther);
+
+addType(Map, processMap);
+addType(Set);
+addType(Int8Array);
+addType(Uint8Array);
+addType(Uint8ClampedArray);
+addType(Int16Array);
+addType(Uint16Array);
+addType(Int32Array);
+addType(Uint32Array);
+addType(Float32Array);
+addType(Float64Array);
+addType(BigInt64Array);
+addType(BigUint64Array);
+addType(DataView);
+addType(ArrayBuffer);
+
+// main
 
 const assemble = (source, env, options) => {
   options = options || empty;
