a963d550f2883eb6416c23ec5c3b7c3c1278c9e0
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-03 22:55:35 -0600

    Added more types.

--- src/utils/deref.js
+++ src/utils/deref.js
@@ -1,4 +1,4 @@
-import {Unifier, Variable, _} from '../unify.js';
+import {Unifier, Variable} from '../unify.js';
 import walk from './walk.js';
 
 const empty = {};
@@ -17,12 +17,31 @@ function postProcess(context) {
 }
 
 const processObject = (val, context) => {
-  if (val === _) {
-    context.stackOut.push(val);
+  const stack = context.stack;
+  stack.push(new walk.Command(postProcess, val));
+  Object.keys(val).forEach(k => stack.push(val[k]));
+};
+
+function postProcessMap(context) {
+  const stackOut = context.stackOut,
+    s = this.s;
+  let j = stackOut.length - 1;
+  for (const key of s) {
+    s.set(key, stackOut[j--]);
+  }
+  const l = stackOut.length - 1 - j;
+  if (l) {
+    stackOut.splice(-l, l, s);
   } else {
-    const stack = context.stack;
-    stack.push(new walk.Command(postProcess, val));
-    Object.keys(val).forEach(k => stack.push(val[k]));
+    stackOut.push(s);
+  }
+}
+
+const processMap = (val, context) => {
+  const stack = context.stack;
+  stack.push(new walk.Command(postProcessMap, val));
+  for (const value of val.values()) {
+    stack.push(value);
   }
 };
 
@@ -35,13 +54,9 @@ const registry = [
     },
     Array,
     function processArray(val, context) {
-      if (val === _) {
-        context.stackOut.push(val);
-      } else {
-        const stack = context.stack;
-        stack.push(new walk.Command(postProcess, val));
-        Object.keys(val).forEach(k => stack.push(val[k]));
-      }
+      const stack = context.stack;
+      stack.push(new walk.Command(postProcess, val));
+      Object.keys(val).forEach(k => stack.push(val[k]));
     },
     Variable,
     function processVariable(val, context) {
@@ -60,6 +75,28 @@ const registry = [
     processOther
   ],
   filters = [];
+
+// add more exotic types
+
+const addType = (Type, process) => typeof Type == 'function' && registry.push(Type, process || processOther);
+
+addType(Map, processMap);
+addType(Set);
+addType(Int8Array);
+addType(Uint8Array);
+addType(Uint8ClampedArray);
+addType(Int16Array);
+addType(Uint16Array);
+addType(Int32Array);
+addType(Uint32Array);
+addType(Float32Array);
+addType(Float64Array);
+addType(BigInt64Array);
+addType(BigUint64Array);
+addType(DataView);
+addType(ArrayBuffer);
+
+// main
 
 const deref = (source, env, options) => {
   options = options || empty;
