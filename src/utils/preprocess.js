7a1dd36b3010833328b619ba8af4f73abc314397
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-03 22:15:00 -0600

    Added support for more types.

--- src/utils/preprocess.js
+++ src/utils/preprocess.js
@@ -23,6 +23,29 @@ const processObject = (val, context) => {
   }
 };
 
+const postProcessMap = (wrapper) =>
+  function (context) {
+    const stackOut = context.stackOut,
+      wrap = context[wrapper],
+      t = new Map();
+    for (const key of this.s.keys()) {
+      t.set(key, stackOut.pop());
+    }
+    stackOut.push(wrap ? wrap(t) : t);
+  };
+
+const processMap = (val, context) => {
+  if (val === _) {
+    context.stackOut.push(val);
+  } else {
+    const stack = context.stack;
+    stack.push(new walk.Command(postProcessMap('wrapMap'), val));
+    for (const key of val.keys()) {
+      stack.push(val.get(key));
+    }
+  }
+};
+
 const processOther = (val, context) => context.stackOut.push(val);
 
 const registry = [
@@ -51,6 +74,28 @@ const registry = [
   ],
   filters = [];
 
+// add more exotic types
+
+const addType = (Type, process) => typeof Type == 'function' && registry.push(Type, process || processOther);
+
+addType(Map, processMap);
+addType(Set);
+addType(Int8Array);
+addType(Uint8Array);
+addType(Uint8ClampedArray);
+addType(Int16Array);
+addType(Uint16Array);
+addType(Int32Array);
+addType(Uint32Array);
+addType(Float32Array);
+addType(Float64Array);
+addType(BigInt64Array);
+addType(BigUint64Array);
+addType(DataView);
+addType(ArrayBuffer);
+
+// main
+
 const preprocess = (source, options) => {
   options = options || empty;
 
@@ -59,9 +104,11 @@ const preprocess = (source, options) => {
   context.stackOut = stackOut;
   context.wrapObject = options.openObjects && open;
   context.wrapArray = options.openArrays && open;
+  context.wrapMap = options.openMaps && open;
 
   walk(source, {
     processObject: options.processObject || processObject,
+    processMap: options.processMap || processMap,
     processOther: options.processOther || processOther,
     registry: options.registry || preprocess.registry,
     filters: options.filters || preprocess.filters,
