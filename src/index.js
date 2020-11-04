4f000ca67515b8e332c5089ef8cfe5d6c8a87658
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-04 00:47:10 -0600

    Exported clone().

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import {any, _} from './env.js';
 import unify from './unify.js';
+import clone from './utils/clone.js';
 import preprocess from './utils/preprocess.js'
 
 const equal = (a, b) => !!unify(a, b);
 
 const match = (object, pattern) => !!unify(object, preprocess(pattern, {openObjects: true}));
 
-export {match, any, _};
+export {clone, match, any, _};
 export default equal;
