42d32046e96f7ff7df5b53a501732092c5f75981
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-04 20:42:49 -0600

    Added "circular" to default options.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import unify from './unify.js';
 import clone from './utils/clone.js';
 import preprocess from './utils/preprocess.js';
 
-const equal = (a, b) => !!unify(a, b);
+const defaultEqualOptions = {circular: true};
+const equal = (a, b) => !!unify(a, b, null, defaultEqualOptions);
 
-const defaultMatchOptions = {openObjects: true, openMaps: true, openSets: true};
-const match = (object, pattern, options = defaultMatchOptions) => !!unify(object, preprocess(pattern, options));
+const defaultMatchOptions = {openObjects: true, openMaps: true, openSets: true, circular: true};
+const match = (object, pattern, options = defaultMatchOptions) => !!unify(object, preprocess(pattern, options), null, {circular: options.circular});
 
 export {clone, match, match as isShape, any, _};
 export default equal;
