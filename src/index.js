89b6088ff7814079053300864821fe20c11d34ae
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-07 18:16:27 -0600

    Improved the simple API.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import originalClone from './traverse/clone.js';
 import preprocess from './traverse/preprocess.js';
 
 const defaultOptions = {circular: true};
-const equal = (a, b) => !!unify(a, b, null, defaultOptions);
+const equal = (a, b, options = defaultOptions) => !!unify(a, b, null, options);
 
 const defaultMatchOptions = {openObjects: true, openMaps: true, openSets: true, circular: true};
 const match = (object, pattern, options = defaultMatchOptions) => !!unify(object, preprocess(pattern, options), null, {circular: options.circular});
 
-const clone = a => originalClone(a, null, defaultOptions);
+const clone = (a, options = defaultOptions) => originalClone(a, null, options);
 
-export {clone, match, match as isShape, any, _};
+export {equal, clone, match, match as isShape, any, _};
 export default equal;
