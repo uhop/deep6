749fc0a58cc11dcfa6a64072c880675361a56318
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-09 14:16:23 -0600

    Extended the list of supported options.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ const defaultOptions = {circular: true};
 const equal = (a, b, options = defaultOptions) => !!unify(a, b, null, options);
 
 const defaultMatchOptions = {openObjects: true, openMaps: true, openSets: true, circular: true};
-const match = (object, pattern, options = defaultMatchOptions) => !!unify(object, preprocess(pattern, options), null, {circular: options.circular});
+const match = (object, pattern, options = defaultMatchOptions) =>
+  !!unify(object, preprocess(pattern, options), null, {
+    circular: options.circular,
+    symbols: options.symbols,
+    loose: options.loose,
+    ignoreFunctions: options.ignoreFunctions
+  });
 
 const clone = (a, options = defaultOptions) => originalClone(a, null, options);
 
